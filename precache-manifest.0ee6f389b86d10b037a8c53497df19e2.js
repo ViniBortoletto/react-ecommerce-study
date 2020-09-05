6fa2a1917cb98e7ba299349341dafb78e4a5e5ba
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2020-09-05 00:32:22 -0300

    Updates

---
+++ precache-manifest.0ee6f389b86d10b037a8c53497df19e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93108d45b1869b25062746f0872b244",
+    "url": "/react-ecommerce-study/index.html"
+  },
+  {
+    "revision": "f67a6ab0dfb6997ce85a",
+    "url": "/react-ecommerce-study/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f40b438cd39e0601949e",
+    "url": "/react-ecommerce-study/static/css/main.72865641.chunk.css"
+  },
+  {
+    "revision": "f67a6ab0dfb6997ce85a",
+    "url": "/react-ecommerce-study/static/js/2.0d2099c4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-ecommerce-study/static/js/2.0d2099c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40b438cd39e0601949e",
+    "url": "/react-ecommerce-study/static/js/main.61f2f3fb.chunk.js"
+  },
+  {
+    "revision": "b02835297ac94c45a98b",
+    "url": "/react-ecommerce-study/static/js/runtime-main.aef9d11a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-ecommerce-study/static/media/logo.9f7e506d.svg"
+  }
+]);
